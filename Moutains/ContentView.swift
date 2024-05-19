34558074c4d2e6af947169c82d207d82e1787143
Tim Mitra <tim@it-guy.com>
2024-05-19 01:55:37 -0400

    Initial Commit

---
+++ Moutains/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Moutains
+//
+//  Created by Tim Mitra on 2024-05-19.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Moutains/MoutainsApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoutainsApp.swift
+//  Moutains
+//
+//  Created by Tim Mitra on 2024-05-19.
+//
+
+import SwiftUI
+
+@main
+struct MoutainsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
